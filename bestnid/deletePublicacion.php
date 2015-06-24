4535c72e749e738b63ded8dc5788c9e6e11cdb92
amarodarchez <amarodarchez@hotmail.com>
2015-06-24 02:12:57 -0300

    Update deletePublicacion.php

--- bestnid/deletePublicacion.php
+++ bestnid/deletePublicacion.php
@@ -3,7 +3,7 @@
 	session_start();
 	$id=$_GET['idprod'];
 	mysql_query("DELETE FROM publicacion WHERE id_publicacion='$id'");
-	echo '<script language="javascript">alert("Producto eliminado con exito");</script>';
-	header("Location: ../profile.php");
+	$msj="Producto eliminado con exito!";
+	header("Location: ../myproductos.php?mensaje=$msj");
 	mysql_close();
 ?>
