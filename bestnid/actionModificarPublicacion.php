3c2cb4916e0f6ce69f3ee2365a5ae3aa47ae30ea
amarodarchez <amarodarchez@hotmail.com>
2015-06-24 02:12:18 -0300

    Create actionModificarPublicacion.php

---
+++ bestnid/actionModificarPublicacion.php
@@ -0,0 +1,18 @@
+<?php
+ 	session_start();
+  	include('connect.php');
+	$name="";
+	$desc="";
+	$id=$_GET['id'];
+	if(!empty($_POST['nombre']) && !empty($_POST['desc'])){
+		$msj="Los datos del producto se modificaron con exito!";
+		$name=$_POST['nombre'];
+		$desc=$_POST['desc'];
+		mysql_query( "UPDATE publicacion SET nombre = '$name', descripcion = '$desc' WHERE id_publicacion = '$id'");
+		header("Location: producto.php?id=$id&mensaje=$msj");
+	}
+	else{
+		$msj="*No se recibieron datos para modificar el producto. Complete el formulario y vuelva a intentarlo.";
+		header("Location: modificar_publicacion.php?id=$id&mensaje=$msj");
+	}
+?>
