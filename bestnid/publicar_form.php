890bb6c00f37add202c81654bbb099a943bc8b23
amarodarchez <amarodarchez@hotmail.com>
2015-07-14 07:01:47 -0300

    Update publicar_form.php

--- bestnid/publicar_form.php
+++ bestnid/publicar_form.php
@@ -1,4 +1,6 @@
-<?php session_start(); ?>
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,13 +12,13 @@
     <meta name="author" content="">
     <link rel="icon" href="../../bestnid-logo.png">
 
-    <title>BestNid</title>
+    <title>BestNid - Registrarse</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="jumbotron.css" rel="stylesheet">
+    <link href="../bootstrap/css/signin.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -31,65 +33,59 @@
 
   <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-	  <b class="navbar-brand navbar-right" id="bienvenido"><i><?php echo $_SESSION['login_user']; ?></i></b>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-	   <b class="navbar-brand navbar-right" id="logout"><a href="logout.php">Cerrar Sesion</a></b>	
-	   <b class="navbar-brand navbar-right" id="products"><a href="myproductos.php">Mis productos</a></b>
-	   <b class="navbar-brand navbar-right" id="products"><a href="#">Datos Personales</a></b>
-        </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h2>Publicar producto</h2>
-        <p>Completa los siguientes datos del producto:</p>
-		<form enctype="multipart/form-data" action='publicar.php' method='POST'>
-		<input type="text" name='producto' placeholder="Nombre del producto" class="form-control">
-		<br>
-		<textarea name="desc" placeholder="Descripcion" class="form-control"></textarea>
-		<br>
-		<h4>Seleccionar foto</h4>
-		<input name="foto" type="file">
-		<br>
-		<?php if(isset($_GET['mensaje'])){ ?>
-				<h3 Style="color:#D00909"><?php echo $_GET['mensaje']; ?></h3>
-		<?php } ?>
-		<br>
-		<br>
-		<button class="btn btn-primary btn-lg" type="submit" class="form-control">Publicar &raquo;</button>
-		</form>
-	<br>
-      </div>
-    </div>
-
     <div class="container">
-      <!-- Example row of columns -->
-	<a class="btn btn-primary btn-lg" href="profile.php" role="button">Volver &raquo;</a>
-      <hr>
+      <form class="form-signin" method="POST" action="register.php">
+        <h2 class="form-signin-heading">Completa los datos</h2>
+		<label for="username" class="sr-only">Nombre de usuario</label>
+		<input type="text" name="username" class="form-control" placeholder="Nombre de usuario" required autofocus>
+        <label for="inputEmail" class="sr-only">Correo Electronico</label>
+        <input type="email" id="inputEmail" name="email" class="form-control" placeholder="Correo Electronico" required>
+		<label for="inputName" class="sr-only">Nombre</label>
+        <input type="text" id="inputName" name="name" class="form-control" placeholder="Nombre" required>
+		<label for="inputLastName" class="sr-only">Apellido</label>
+        <input type="text" id="inputLastName" name="lastName" class="form-control" placeholder="Apellido" required>
+		<label for="inputDni" class="sr-only">DNI</label>
+        <input type="number" id="inputDni" name="dni" maxlength=7 class="form-control" placeholder="DNI" required>
+		<label for="inputCity" class="sr-only">Localidad</label>
+        <input type="text" id="inputCity" name="city" class="form-control" placeholder="Localidad" required>
+		<label for="inputStreet" class="sr-only">Calle</label>
+        <input type="text" id="inputStreet" name="street" class="form-control" placeholder="Calle" required>
+		<label for="inputNumber" class="sr-only">Número</label>
+        <input type="number" id="inputNumber" name="number" class="form-control" placeholder="Número" required>
+		<label for="inputDepto" class="sr-only">Depto</label>
+        <input type="text" id="inputDepto" name="depto" class="form-control" placeholder="Depto">
+		<label for="inputFloor" class="sr-only">Piso</label>
+        <input type="text" id="inputFloor" name="floor" class="form-control" placeholder="Piso">		
+        <label for="inputPassword" class="sr-only">Contraseña</label>
+        <input type="password" id="inputPassword" name="pass1" class="form-control" placeholder="Contraseña " required>
+        <label for="inputPassword2" class="sr-only">Contraseña2</label>
+        <input type="password" id="inputPassword2" name="pass2" class="form-control" placeholder="Repetir Contraseña" required>
+		<br>
+        <button name="send" id="send" class="btn btn-lg btn-primary btn-block" type="submit">Registrarse</button>
+		<?php if(isset($_SESSION['login_user'])){ ?>
+						<button class="btn btn-lg btn-primary btn-block" type="submit" onclick="window.location.href='gestionUsuarios.php'">Cancelar</button> <?php
+				}else{ ?>
+					<button class="btn btn-lg btn-primary btn-block" type="submit" onclick="window.location.href='index.php'">Cancelar</button> <?php } ?>
+      </form>
 
+		<?php 
+			if(empty($_POST) && !empty($_SESSION['errors'])){ ?>
+				<div>
+				<?php
+				foreach($_SESSION['errors'] as $value){ ?>
+					<br><b><?php echo '*' .$value; ?></b>
+				<?php }?>
+				</div>
+			<?php
+			}		
+		?>
+
+    </div> <!-- /container -->
+	<hr>
       <footer>
         <p align="center">&copy; Desarrollado por CBD</p>
       </footer>
-    </div> <!-- /container -->
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
