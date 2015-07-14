4990de1dcc7aa9d77c43a28effd791df19ffbf41
amarodarchez <amarodarchez@hotmail.com>
2015-07-14 06:47:03 -0300

    Update actionModificarPublicacion.php

--- bestnid/actionModificarPublicacion.php
+++ bestnid/actionModificarPublicacion.php
@@ -4,15 +4,14 @@
 	$name="";
 	$desc="";
 	$id=$_GET['id'];
-	if(!empty($_POST['nombre']) && !empty($_POST['desc'])){
+	if(!empty($_POST['nombre']) || !empty($_POST['desc'])){
 		$msj="Los datos del producto se modificaron con exito!";
 		$name=$_POST['nombre'];
 		$desc=$_POST['desc'];
 		mysql_query( "UPDATE publicacion SET nombre = '$name', descripcion = '$desc' WHERE id_publicacion = '$id'");
-		header("Location: producto.php?id=$id&mensaje=$msj");
 	}
 	else{
 		$msj="*No se recibieron datos para modificar el producto. Complete el formulario y vuelva a intentarlo.";
-		header("Location: modificar_publicacion.php?id=$id&mensaje=$msj");
 	}
+	header("Location: modificar_publicacion.php?id=$id&mensaje=$msj");
 ?>
