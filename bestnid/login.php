93ea93f1196b01e461f92ae8b2a961b6805664ea
amarodarchez <amarodarchez@hotmail.com>
2015-07-14 06:53:48 -0300

    Update login.php

--- bestnid/login.php
+++ bestnid/login.php
@@ -1,21 +1,32 @@
 <?php
 	include_once('connect.php');
 	session_start();
+	$bloq=0;
 	$username=$_POST['username'];
 	$password=$_POST['password'];	
 	$sql="SELECT * FROM usuario WHERE nombre_usuario='$username' and password='$password'";
 	$result=mysql_query($sql);
 	$rows=mysql_num_rows($result);
 	if($rows!=0){
-		$_SESSION['login_user']=$username; 
 		$row=mysql_fetch_assoc($result);
-		if ($row['admin']==0){
-			$_SESSION['nivel']=0;
+		if($row['admin']==2){
+			$bloq=1;
 		}
 		else{
-			$_SESSION['nivel']=1;
+			$_SESSION['login_user']=$username; 
+			if ($row['admin']==0){
+				$_SESSION['nivel']=0;
+			}
+			else{
+				$_SESSION['nivel']=1;
+			}
 		}
-		header("location:index.php");
+		if($bloq==0){
+			header("Location:index.php");	
+		}
+		else{
+			header("Location:index.php?bloq=El usuario se encuentra bloqueado");	
+		}
 	}else {
 		?>
 		<script language="javascript">window.location='index.php?msg=Nombre de usuario o contraseña incorrecta!';</script>
